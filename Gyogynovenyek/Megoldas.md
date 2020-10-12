7482542dd7fd28c7e94d6956c8913458e25f8993
MakkaiPeti01 <PetyaPower@gmail.com>
2020-10-12 12:09:03 +0200

    Orai munka, gyakorlasra

--- Gyogynovenyek/Megoldas.md
+++ Gyogynovenyek/Megoldas.md
@@ -1,8 +1,47 @@
 ﻿# Megoldás
 
 # Osztály létrehozása
+- Kétféle mód közül bármelyik
+  - CTRL + .
+  - Adjunk hozzá Itemet
 - 5 tulajdonság
   - public get
   - private set 
 - 1 konstruktor
   - 1 paraméter (string)
+  - például "Acsalapu;levél;6;8"
+  - ";" vel vannak elválasztva
+  - 4 darab adat --> 5 tulajdonság?
+  - átadott, fájlból beolvasott sort:
+    - ";" mentén tagoljuk
+    - [0] --> Nev
+    - [1] --> Resz
+    - [2] --> Kezd
+    - [3] --> Veg
+    - Idotartam --> Számolás
+- 5.tulajdonság (időtartam)
+    - Konstruktorban számoljuk
+    - Számolása: vég-kezdet HA vég > kezdet
+    - Vég > Kezdet --> Vég-Kezdet+1
+    - Egyébként --> 12-Kezdet+Vég+1
+    
+## Adatok beolvasása és tárolása
+- Listában tároljuk az objektum példányokat
+- Olvassuk be:
+  - StreamReader (System.IO)
+  - while(EndOfStream)
+  - Cikluson belül hozzáadunk egy új objektum példányt (Beolvasott sor + paraméter)
+
+## 1. Feladat
+- Hány növény van? (lista hossza)
+## 2. Feladat
+- Milyen gyűjthető részek vannak
+  - Foreach Növények
+  - Ha a rész nem kulcs --> akkor hozzáadjuk 0-val
+- Hány db van ezekből
+  - foreach novenyek, ha resz == Key akkor Value ++
+## 3. Feladat
+- Maximum keresés időtartam alapján
+- Növények, amelyek a legtöbb idejig szedhetőek
+## 4. Feladat
+- Átlag időtartam számítás (Összegzés)
--- Gyogynovenyek/Program.cs
+++ Gyogynovenyek/Program.cs
@@ -9,9 +9,72 @@ namespace Gyogynovenyek
 {
     class Program
     {
+        static List<Noveny> lista = new List<Noveny>();
+        static Dictionary<string, int> reszek = new Dictionary<string, int>();
+        static void Beolvas()
+        {
+            StreamReader olvas = new StreamReader("noveny.csv");
+            while (!olvas.EndOfStream)
+            {
+                lista.Add(new Noveny(olvas.ReadLine()));
+            }
+            olvas.Close();
+            Console.WriteLine("1. feladat: Növények száma: {0}", lista.Count);
+        }
+        static void Masodik()
+        {
+            Console.WriteLine("\n2. feladat: Gyűjthető részek:");
+            foreach (var i in lista)
+            {
+                if (!reszek.ContainsKey(i.Resz))
+                {
+                    reszek.Add(i.Resz, 0);
+                }
+            }
+            foreach (var n in lista)
+            {
+                reszek[n.Resz]++;
+            }
+            foreach (var i in reszek)
+            {
+                Console.WriteLine("{0} {1}",i.Key,i.Value);
+            }
+        }
+        static void Harmadik()
+        {
+            int max = 0;
+            string noveny = "";
+            foreach (var i in lista)
+            {
+                if (i.Idotartam > max)
+                {
+                    max = i.Idotartam;
+                    noveny = i.Nev;
+                }
+            }
+                Console.WriteLine("3. feladat: Legtöbb idő, amíg gyűjthető: {0}",max);
+                Console.Write("Növény(ek): {0}", noveny);
+        }
+        static void Negyedik()
+        {          
+            double s = 0;
+            double atlag = 0;
+            foreach (var i in lista)
+            {
+                s = s + i.Idotartam;
+            }
+            atlag = Math.Round(s / lista.Count, 2);
+            Console.WriteLine("");
+            Console.WriteLine("4. feladat: {0}", atlag);
+        }
         static void Main(string[] args)
         {
-
+            var N = new Noveny("Acsalapu;levél;6;8");
+            Beolvas();
+            Masodik();
+            Harmadik();
+            Negyedik();
+            Console.ReadKey();
         }
     }
 }
